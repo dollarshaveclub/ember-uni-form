e41254068c3040ed94793fb6cd3d9dd6226ee447
Patrick Fisher <patrick@pwfisher.com>
2014-05-16 14:34:02 -0700

    camelCase Ember model properties

--- app/models/address-model.js
+++ app/models/address-model.js
@@ -1,8 +1,8 @@
 App.Address = DS.Model.extend({
-  address_line_1: DS.attr('string'),
-  address_line_2: DS.attr('string'),
+  addressLine1: DS.attr('string'),
+  addressLine2: DS.attr('string'),
   city: DS.attr('string'),
   state: DS.attr('string'),
   country: DS.attr('string'),
-  postal_code: DS.attr('string')
+  postalCode: DS.attr('string')
 });
