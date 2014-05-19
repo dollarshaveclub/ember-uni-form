15d536fe6f39691435bd551d690e171c52c189ca
Patrick Fisher <patrick@pwfisher.com>
2014-05-19 14:14:03 -0700

    camelCase fix for addressLine_1

--- app/models/address-model.js
+++ app/models/address-model.js
@@ -1,6 +1,6 @@
 App.Address = DS.Model.extend({
-  addressLine1: DS.attr('string'),
-  addressLine2: DS.attr('string'),
+  addressLine_1: DS.attr('string'),
+  addressLine_2: DS.attr('string'),
   city: DS.attr('string'),
   state: DS.attr('string'),
   country: DS.attr('string'),
