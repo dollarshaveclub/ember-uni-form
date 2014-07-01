178fdefa566af13b9fc9412d74f60a50bd308559
Brian Gonzalez <me@briangonzalez.org>
2014-07-01 07:58:56 -0700

    issue #133 - cleaned up quantity widget

--- app/components/quantity-widget-component.js
+++ app/components/quantity-widget-component.js
@@ -4,31 +4,31 @@ var MAX_QTY = 3; // to be replaced with dynamic value variable
 App.QuantityWidgetComponent = Ember.Component.extend({
 
   tagName: 'div',
-  classNameBindings: [':qty-widget', 'isMinQty', 'isMaxQty'],
+  classNameBindings: [':quantity-widget', 'isMin', 'isMax'],
   attributeBindings:['name:data-icon'],
 
   actions: {
     increment: function () {
-      if ( this.getQty() < MAX_QTY )
-        this.set('quantity', this.getQty() + 1);
+      if ( this.getQuantity() < MAX_QTY )
+        this.set('quantity', this.getQuantity() + 1);
     },
 
     decrement: function () {
-      if ( this.getQty() > 1 )
-        this.set('quantity', this.getQty() - 1);
+      if ( this.getQuantity() > 1 )
+        this.set('quantity', this.getQuantity() - 1);
     }
   },
 
-  getQty: function() {
+  getQuantity: function() {
     return this.get('quantity');
   },
 
-  isMinQty: function() {
-    return this.getQty() === 1;
+  isMin: function() {
+    return this.getQuantity() === 1;
   }.property('quantity'),
 
-  isMaxQty: function() {
-    return this.getQty() >= MAX_QTY;
+  isMax: function() {
+    return this.getQuantity() >= MAX_QTY;
   }.property('quantity')
 
 });
