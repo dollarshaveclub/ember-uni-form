092dfaa9887d1ca25efd923d1b0d54f2324cece3
Jason Farmer <jason.farmer@dollarshaveclub.com>
2014-10-27 09:14:36 -0700

    issue 496 - Address form select component

--- app/components/validated-select-component.js
+++ app/components/validated-select-component.js
@@ -1,39 +1,27 @@
 
 App.ValidatedSelectComponent = Ember.Select.extend({
 
-  classNameBindings: ['error', 'open'],
+  classNameBindings: ['error'],
+
+  observeErrors: function () {
+    this.get('parentModel').addObserver('errors.' + this.get('name'), this, this.syncErrors);
+  }.on('didInsertElement'),
+
+  syncErrors: function () {
+    this.set('errors', this.get('parentModel.errors.' + this.get('name')));
+  },
 
   focusOut: function () {
     this.set('error', this.get('isInvalid'));
-    this.set('open', false);
   },
 
-  click: function() {
-    this.toggleProperty('open');
-  },
-
-  // change: function() {
-  //   this.set('open', true);
-  //   console.log("opened");
-  // },
-
-  keyDown: function() {
-    this.set('open', true);
-  },
-
-  errors: function () {
-    var errorName = "parentModel.errors.%@".fmt( this.get('name') );
-    return this.get(errorName);
-  }.property('name', 'parentModel.isValid'),
-
-  isValid: function () {
-    if ( this.get('errors') ) {
-      return this.get('errors').length === 0;
-    } else {
-      return true;
+  keyUp: function () {
+    if ( this.get('isValid') ) {
+      this.set('error', false);
     }
-  }.property('name', 'parentModel.isValid'),
+  },
 
-  isInvalid: Ember.computed.not('isValid')
+  isValid: Ember.computed.empty('errors'),
+  isInvalid: Ember.computed.notEmpty('errors')
 
 });
