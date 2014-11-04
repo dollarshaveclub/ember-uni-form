e721c0d4d957006b2cece37433a05ae6535e98e7
Jason Farmer <jason.farmer@dollarshaveclub.com>
2014-11-04 13:22:36 -0800

    Issue #556 - month and year select components

---
+++ app/components/validated-select-month-component.js
@@ -0,0 +1,4 @@
+
+App.ValidatedSelectMonthComponent = App.ValidatedSelectComponent.extend({
+  content: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+});
---
+++ app/components/validated-select-year-component.js
@@ -0,0 +1,15 @@
+
+App.ValidatedSelectYearComponent = Ember.Select.extend({
+
+  years: [],
+
+  content: function() {
+    var years    = this.get('years'),
+        thisYear = new Date().getFullYear();
+    for ( var i = 0; i < 12; i++ ) {
+      years.push(thisYear + i);
+    }
+    return years;
+  }.property('years')
+
+});
