d47df6a8781e2439c588cbdff23d5d553a07c385
Jason Farmer <jason.farmer@dollarshaveclub.com>
2014-11-06 16:17:19 -0800

    Issue #556 - default expiration month to current month

--- app/components/validated-select-month-component.js
+++ app/components/validated-select-month-component.js
@@ -1,4 +1,15 @@
 
 App.ValidatedSelectMonthComponent = App.ValidatedSelectComponent.extend({
-  content: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+  content: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+
+  didInsertElement: function() {
+    this._super();
+    if ( this.get('parentModel.expMonth') ) return;
+
+    var month = moment().month() + 1;
+    var value = month > parseInt(this.get('value')) ? month : this.get('value');
+    this.set( 'value', value.toString() );
+  }
+
 });
