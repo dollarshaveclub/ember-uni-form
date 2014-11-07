7a05e7f020153745b6190eef7cbad3ac2637d6bb
Jason Farmer <jason.farmer@dollarshaveclub.com>
2014-11-07 10:00:31 -0800

    Issue #556 - setting models for payment method

--- app/models/address-model.js
+++ app/models/address-model.js
@@ -10,6 +10,14 @@ App.Address = DS.Model.extend(
   state: DS.attr('string'),
   zipCode: DS.attr('string'),
 
+  // TODO: How do we get at locales in a better fashion than this?
+  locale: function () {
+    var self = this;
+    return this.store.find('user', 'me').then(function(user){
+      return self.set('locale', user.get('locale'));
+    });
+  }.on('init'),
+
   name: function () {
     return this.get('firstName') + ' ' + this.get('lastName');
   }.property('firstName', 'lastName'),
