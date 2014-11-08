ebe734f6004fb3d303d02be97b22e8e4525d3a7c
Brian Gonzalez <me@briangonzalez.org>
2014-11-08 11:20:15 -0800

    Refactor alert/notificationlogic into HandlesAlertsAndNotifications mixin

---
+++ app/mixins/handles-alerts-and-notifications.js
@@ -0,0 +1,47 @@
+
+App.HandlesAlertsAndNotifications = Ember.Mixin.create({
+
+  initAssertion: function () {
+    Ember.assert(
+      'HandlesAlertsAndNotifications must be mixed in to the ApplicationRoute',
+      Object.getPrototypeOf(this)._debugContainerKey === "route:application"
+    );
+  }.on('init'),
+
+  actions: {
+
+    alert: function (message) {
+      this.send('removeNotification');
+      this.send('openModal', 'alert', Ember.Object.create({ title: message }));
+    },
+
+    notify: function (opts) {
+      this.send('removeNotification');
+
+      var message  = (typeof opts === 'string') ? opts : (opts.message || 'No message given.');
+
+      this.controllerFor('notification').setProperties({
+        message: message,
+        type: opts.type,
+        dismissable: opts.dismissable
+      });
+
+      var view = this.render('notification', {
+        into: 'application',
+        outlet: 'notification',
+        controller: 'notification'
+      });
+    },
+
+    removeNotification: function () {
+      this.controllerFor('notification').set('isShown', false);
+
+      return this.disconnectOutlet({
+        outlet: 'notification',
+        parentView: 'application'
+      });
+    }
+
+  }
+
+});
