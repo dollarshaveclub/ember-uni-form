68ce235416de27244478fd3913e0bcf2dc1b197a
Jason Farmer <jason.farmer@dollarshaveclub.com>
2014-11-07 16:47:01 -0800

    Issue #556 - 2 digit year for credit card expiration year options

--- app/components/validated-select-year-component.js
+++ app/components/validated-select-year-component.js
@@ -7,7 +7,7 @@ App.ValidatedSelectYearComponent = Ember.Select.extend({
     var years    = this.get('years'),
         thisYear = new Date().getFullYear();
     for ( var i = 0; i < 12; i++ ) {
-      years.push( (thisYear + i).toString() );
+      years.push( (thisYear + i).toString().substr(2,3) );
     }
     return years;
   }.property('years')
