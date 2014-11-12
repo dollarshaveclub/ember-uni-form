0c39c9c716cd8aa546cf9bec0378c05247f43a5a
Jason Farmer <jason.farmer@dollarshaveclub.com>
2014-11-11 17:29:26 -0800

    Issue #556 - fix years select content generation

--- app/components/validated-select-year-component.js
+++ app/components/validated-select-year-component.js
@@ -5,15 +5,19 @@ var YEAR_PREFIX = '20';
 
 App.ValidatedSelectYearComponent = App.ValidatedSelectComponent.extend({
 
-  years: [],
-
   content: function() {
-    var years    = this.get('years'),
+
+    var years    = [],
         thisYear = new Date().getFullYear();
+
+    if ( years.length > 12 ) return;
+
     for ( var i = 0; i < 12; i++ ) {
       years.push( YEAR_PREFIX + (thisYear + i).toString().substr(2,3) );
     }
+
     return years;
-  }.property('years')
+
+  }.property()
 
 });
