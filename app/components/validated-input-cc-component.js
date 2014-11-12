7277fcfbb2339ac46fe0a4f3afdde4397dfe2f37
Brian Gonzalez <me@briangonzalez.org>
2014-11-11 22:53:40 -0800

    Issue #556 - better bindings for jquery.payment on validated-cc/cvc input component

--- app/components/validated-input-cc-component.js
+++ app/components/validated-input-cc-component.js
@@ -3,20 +3,12 @@ require('../components/validated-input-component');
 
 App.ValidatedInputCcComponent = App.ValidatedInputComponent.extend({
 
-  change: function (e) {
-    this.formatCcNumber(e);
-  },
+  attributeBindings: [ 'pattern', 'autocomplete' ],
+  autocomplete: "cc-number",
+  pattern: "\d*",
 
-  keyUp: function (e) {
-    this._super(e);
-    this.formatCcNumber(e);
-  },
-
-  formatCcNumber: function(e) {
-    var $target = $(e.target);
-    if ( $target.attr('name') === 'number' ) {
-      $target.payment('formatCardNumber');
-    }
-  }
+  formatCC: function() {
+    this.$().payment('formatCardNumber');
+  }.on('didInsertElement')
 
 });
--- app/components/validated-input-cvv-component.js
+++ app/components/validated-input-cvv-component.js
@@ -3,20 +3,12 @@ require('../components/validated-input-component');
 
 App.ValidatedInputCvvComponent = App.ValidatedInputComponent.extend({
 
-  change: function (e) {
-    this.formatCvvNumber(e);
-  },
+  attributeBindings: [ 'pattern', 'autocomplete' ],
+  autocomplete: "off",
+  pattern: "\d*",
 
-  keyUp: function (e) {
-    this._super(e);
-    this.formatCvvNumber(e);
-  },
-
-  formatCvvNumber: function(e) {
-    var $target = $(e.target);
-    if ( $target.attr('name') === 'cvv' ) {
-      $target.payment('formatCardCVC');
-    }
-  }
+  formatCVV: function() {
+    this.$().payment('formatCardCVC');
+  }.on('didInsertElement')
 
 });
