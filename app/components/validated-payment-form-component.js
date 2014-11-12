99e8dfa39789b3a82d671f664e322be157b641eb
Jason Farmer <jason.farmer@dollarshaveclub.com>
2014-11-11 18:05:14 -0800

    Issue #556 - fix billing address validations + limit zip to 5 chars

--- app/components/validated-payment-form-component.js
+++ app/components/validated-payment-form-component.js
@@ -3,7 +3,11 @@ require('../components/validated-form-component');
 
 App.ValidatedPaymentFormComponent = App.ValidatedFormComponent.extend({
 
-  classNameBindings: [ 'isValid:valid:invalid', 'cardType' ],
-  cardType: Ember.computed.alias('formModel.creditCardType')
+  classNameBindings: [ 'cardType' ],
+  cardType: Ember.computed.alias('formModel.creditCardType'),
+
+  isValid: function() {
+    return this.get('formModel.isValid') && this.get('formModel.billingAddress.isValid');
+  }.property('formModel.isValid', 'formModel.billingAddress.isValid')
 
 });
