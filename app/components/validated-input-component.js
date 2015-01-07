b30ba8b934768f68cec3ea3fa2649437bf4b44fc
Patrick Fisher <patrick@pwfisher.com>
2015-01-06 16:06:55 -0800

    minor

--- app/components/validated-input-component.js
+++ app/components/validated-input-component.js
@@ -17,20 +17,19 @@ App.ValidatedInputComponent = Ember.TextField.extend({
   },
 
   observeErrors: function () {
-    if ( !this.get('parentModel') ) return;
+    if (!this.get('parentModel')) return;
     this.get('parentModel').addObserver('errors.' + this.get('name'), this, this.syncErrors);
   }.on('didInsertElement'),
 
   required: function () {
-    if ( !this.get('parentModel') ) return;
+    if (!this.get('parentModel')) return;
     var v = this.get('parentModel.validations');
     return v[this.get('name')] && v[this.get('name')].presence;
   }.property('name', 'parentModel.validations'),
 
   syncErrors: function () {
-    if (!this.get('isDestroyed')) {
-      this.set('errors', this.get('parentModel.errors.' + this.get('name')));
-    }
+    if (this.get('isDestroyed')) return;
+    this.set('errors', this.get('parentModel.errors.' + this.get('name')));
   }
 
 });
