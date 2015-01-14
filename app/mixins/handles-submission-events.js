35219fe9ce7ec8a58b31c4c16fd59d679133aed4
Patrick Fisher <patrick@pwfisher.com>
2015-01-13 18:17:20 -0800

    Ajax error handling: no Rollbar error on 401 or 422, temp hack to allow handleSubmissionEvents to work with ajaxError return value

--- app/mixins/handles-submission-events.js
+++ app/mixins/handles-submission-events.js
@@ -3,21 +3,19 @@ App.HandlesSubmissionEvents = Ember.Mixin.create(
 {
 
   handleSubmissionSuccess: function (resolve, response) {
-    // console.log('[HandlesSubmissionEvents Mixin Success]', arguments);
+    console.log('[HandlesSubmissionEvents success]', arguments);
     this.get('store').pushPayload(response);
     resolve(response);
   }.on('submissionSuccess'),
 
   handleSubmissionError: function (reject, response) {
-    // console.log('[HandlesSubmissionEvents Mixin Failure]', arguments);
-
-    if ( response.status === 401 ) {
+    console.log('[HandlesSubmissionEvents error]', arguments);
+    if (response.status === 401 || response.http_status === 401) { // TEMPORARY HACK @see RESTAdapter.ajaxError
       reject({ errors: [ 'Please try again.' ], unauthorized: true });
     } else {
       var errors = JSON.parse(response.responseText).errors || [];
       reject({ errors: errors });
     }
-
   }.on('submissionError')
 
 });
