b201c9d09e757a0de6b1a844874ef46fc8815ff9
Patrick Fisher <patrick@pwfisher.com>
2015-01-13 18:18:23 -0800

    minor

--- app/mixins/handles-submission-events.js
+++ app/mixins/handles-submission-events.js
@@ -2,15 +2,15 @@ App.HandlesSubmissionEvents = Ember.Mixin.create(
   Ember.Evented,
 {
 
-  handleSubmissionSuccess: function (resolve, response) {
+  handleSubmissionSuccess: function (resolve, data) {
     console.log('[HandlesSubmissionEvents success]', arguments);
-    this.get('store').pushPayload(response);
-    resolve(response);
+    this.get('store').pushPayload(data);
+    resolve(data);
   }.on('submissionSuccess'),
 
-  handleSubmissionError: function (reject, response) {
+  handleSubmissionError: function (reject, jqXHR) {
     console.log('[HandlesSubmissionEvents error]', arguments);
-    if (response.status === 401 || response.http_status === 401) { // TEMPORARY HACK @see RESTAdapter.ajaxError
+    if (jqXHR.status === 401) {
       reject({ errors: [ 'Please try again.' ], unauthorized: true });
     } else {
       var errors = JSON.parse(jqXHR.responseText).errors || [];
