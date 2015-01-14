4200c3539915648d5931a71afc5b3aa5565721c6
Brian Gonzalez <me@briangonzalez.org>
2015-01-13 23:41:38 -0800

    minor

--- app/mixins/provides-ui-errors.js
+++ app/mixins/provides-ui-errors.js
@@ -21,14 +21,14 @@ App.ProvidesUIErrors = Ember.Mixin.create(
   // Trigger `shouldShowValidationError` or `dismissValidationError`
   // with a given property to push/remove that name onto shownValidationProperties
 
-  showValidationError: function (name, id) {
+  showValidationError: function (name) {
     if (this.get('propertiesInUIErrorState').contains(name))
       return;
 
     this.get('propertiesInUIErrorState').pushObject(name);
   }.on('shouldShowValidationError'),
 
-  dismissValidationError: function (name, id) {
+  dismissValidationError: function (name) {
     this.get('propertiesInUIErrorState').removeObject(name);
   }.on('shouldDismissValidationError'),
 
