d4cafac2b736930a9aebed04cbc773bb8416e694
Brian Gonzalez <me@briangonzalez.org>
2015-01-13 22:12:19 -0800

    Refined walletBalance and creditsCoverTotal logic

--- app/components/validated-payment-form-checkout-component.js
+++ app/components/validated-payment-form-checkout-component.js
@@ -3,16 +3,13 @@ require('../components/validated-payment-form-component');
 
 App.ValidatedPaymentFormCheckoutComponent = App.ValidatedPaymentFormComponent.extend({
 
-  credits: Ember.computed.alias('currentUser.walletBalance'),
+  walletBalance: Ember.computed.alias('currentUser.walletBalance'),
   total: Ember.computed.alias('currentUser.nextBox.total'),
   showPaymentForm: true,
-
-  creditsCoverTotal: function () {
-    return this.get('credits') > this.get('total');
-  }.property('credits', 'total'),
+  creditsCoverTotal: false,
 
   // For some reason, property does not work here,
-  // so we add an observer below.
+  // so we add observers below.
   watchSkipPaymentMethod: function () {
     if (this.get('creditsCoverTotal')) {
       this.set('showPaymentForm', !this.get('skipPaymentMethod'));
@@ -21,13 +18,16 @@ App.ValidatedPaymentFormCheckoutComponent = App.ValidatedPaymentFormComponent.ex
     }
   }.observes('skipPaymentMethod', 'creditsCoverTotal', 'user.isLoggedIn'),
 
+  watchCreditsAndTotal: function () {
+    this.set('creditsCoverTotal', this.get('walletBalance') > this.get('total'));
+  }.observes('walletBalance', 'total'),
+
   skipPaymentMethod: function () {
     return parseInt(this.get('user.createSubscriptionAction.skipPaymentMethod'));
   }.property('user.createSubscriptionAction.skipPaymentMethod'),
 
-  walletBalance: function () {
-    var walletBalance = this.get('credits') > 0;
-    return walletBalance;
+  hasWalletBalance: function () {
+    return this.get('walletBalance') > 0;
   }.property('credits')
 
 });
