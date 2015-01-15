ca285fd3a149d5a211bc18829dc3278122a82884
Brian Gonzalez <me@briangonzalez.org>
2015-01-15 15:26:49 -0800

    Cleaned up validated-payment-form-checkout-component.js to use computed props; not observers

--- app/components/validated-payment-form-checkout-component.js
+++ app/components/validated-payment-form-checkout-component.js
@@ -1,42 +1,22 @@
-
 require('../components/validated-payment-form-component');
 
 App.ValidatedPaymentFormCheckoutComponent = App.ValidatedPaymentFormComponent.extend({
 
   walletBalance: Ember.computed.alias('currentUser.walletBalance'),
   total: Ember.computed.alias('currentUser.nextBox.total'),
-  showPaymentForm: true,
-  creditsCoverTotal: false,
   purchase: 'purchase',
 
-  actions: {
+  showPaymentForm: function () {
+    if (!this.get('creditsCoverTotal')) return true;
+    return this.get('currentUser.createSubscriptionAction.skipPaymentMethod') === '0';
+  }.property('creditsCoverTotal', 'currentUser.createSubscriptionAction.skipPaymentMethod'),
 
-    submit: function () {
-      this.sendAction('purchase');
-    }
-
-  },
-
-  // For some reason, property does not work here,
-  // so we add observers below.
-  watchSkipPaymentMethod: function () {
-    if (this.get('creditsCoverTotal')) {
-      this.set('showPaymentForm', !this.get('skipPaymentMethod'));
-    } else {
-      this.set('showPaymentForm', true);
-    }
-  }.observes('skipPaymentMethod', 'creditsCoverTotal', 'user.isLoggedIn'),
-
-  watchCreditsAndTotal: function () {
-    this.set('creditsCoverTotal', this.get('walletBalance') > this.get('total'));
-  }.observes('walletBalance', 'total'),
-
-  skipPaymentMethod: function () {
-    return parseInt(this.get('user.createSubscriptionAction.skipPaymentMethod'));
-  }.property('user.createSubscriptionAction.skipPaymentMethod'),
+  creditsCoverTotal: function () {
+    return this.get('walletBalance') > this.get('total');
+  }.property('walletBalance', 'total'),
 
   hasWalletBalance: function () {
     return this.get('walletBalance') > 0;
-  }.property('credits')
+  }.property('walletBalance')
 
 });
