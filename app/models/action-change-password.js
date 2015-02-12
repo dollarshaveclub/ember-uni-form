1f0a82f7603a81c1c1cf74e6f599551aa2ececb5
Patrick Fisher <patrick@pwfisher.com>
2015-02-11 23:23:04 -0800

    /your-account page: cannot open password edit page: Error while processing route: account-password No model was found for 'action-change-password'

---
+++ app/models/action-change-password.js
@@ -0,0 +1,46 @@
+import DS from 'ember-data';
+import EmberValidations from 'ember-validations';
+import HandlesSubmissionEvents from '../mixins/handles-submission-events';
+import ApplicationAdapter from '../adapters/application';
+
+export default DS.Model.extend(
+  EmberValidations.Mixin,
+  HandlesSubmissionEvents,
+{
+
+  currentPassword: DS.attr('string'),
+  newPassword: DS.attr('string'),
+  newPasswordConfirmation: DS.attr('string'),
+
+  validations: {
+    currentPassword: {
+      length: { minimum: 6 },
+      presence: true
+    },
+    newPassword: {
+      confirmation: true,
+      length: { minimum: 6 },
+      presence: true
+    }
+  },
+
+  minLength: Ember.computed.alias('validations.newPassword.length.minimum'),
+
+  submit: function (user) {
+    var self = this;
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      Ember.$.ajax({
+        url: ApplicationAdapter.create().buildURL('user', user.get('id')) + '/change_password',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          user: self.serialize()
+        })
+      }).then(function (data) {
+        self.trigger('submissionSuccess', resolve, data);
+      }, function (jqXHR) {
+        self.trigger('submissionError', reject, jqXHR);
+      });
+    });
+  }
+});
