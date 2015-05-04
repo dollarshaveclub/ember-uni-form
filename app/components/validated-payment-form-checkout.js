ff24e612b09aa346b1824dc6b70245abdb32d690
Jacob Kelley <jakie8@gmail.com>
2015-05-04 15:31:05 -0700

    Only scrolling to billing info if shipping address information has been completely filled out

--- app/components/validated-payment-form-checkout.js
+++ app/components/validated-payment-form-checkout.js
@@ -9,6 +9,24 @@ export default ValidatedPaymentFormComponent.extend({
   total: Ember.computed.alias('currentUser.nextBox.total'),
   walletBalance: Ember.computed.alias('currentUser.walletBalance'),
 
+  shippingAddress: Ember.computed.oneWay('currentUser.subscription.shippingAddress'),
+
+  shippingAddressComplete: function () {
+    return !!(
+      this.get('shippingAddress.firstName') &&
+      this.get('shippingAddress.lastName') &&
+      this.get('shippingAddress.addressLine_1') &&
+      this.get('shippingAddress.city') &&
+      this.get('shippingAddress.state') &&
+      this.get('shippingAddress.zipCode')
+    );
+  }.property( 'shippingAddress.firstName',
+              'shippingAddress.lastName',
+              'shippingAddress.addressLine_1',
+              'shippingAddress.city',
+              'shippingAddress.state',
+              'shippingAddress.zipCode'),
+
   showPaymentForm: function () {
     if (!this.get('creditsCoverTotal')) return true;
     return this.get('skipPaymentMethod') === '0';
