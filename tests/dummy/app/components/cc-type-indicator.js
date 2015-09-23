a568df8061d9e9070ff288373ea7ef89b88797e7
Patrick Fisher <patrick@pwfisher.com>
2015-09-22 19:49:55 -0700

    Remove legacy code with no future

--- tests/dummy/app/components/cc-type-indicator.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-
-  classNames: [ 'cc-type-indicator' ],
-  attributeBindings: [ 'cardType:data-card-type' ]
-
-});
--- tests/dummy/app/components/truthiness-toggler.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-
-  click: function () {
-    this.toggleProperty('toggleProp');
-  }
-
-});
