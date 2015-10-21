606cf4c2c696053e833e8622a3cd2851afbad542
Patrick Fisher <patrick@pwfisher.com>
2015-10-21 01:43:00 -0700

    bump code coverage a bit

--- tests/unit/mixins/has-field-status-test.js
+++ tests/unit/mixins/has-field-status-test.js
@@ -7,3 +7,11 @@ module('mixin:has-field-status');
 test('it works', function (assert) {
   assert.ok(Ember.Object.extend(HasFieldStatus).create());
 });
+
+test('it should set focus on focusIn and reset it on focusOut', function (assert) {
+  var subject = Ember.Object.extend(HasFieldStatus).create();
+  subject.focusIn();
+  assert.equal(subject.get('focus'), true);
+  subject.focusOut();
+  assert.equal(subject.get('focus'), false);
+});
