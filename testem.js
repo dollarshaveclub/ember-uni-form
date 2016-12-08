c138a05b20ca783b68106da795b4db9ebc81906c
Brian Gonzalez <me@briangonzalez.org>
2016-12-07 17:21:26 -0800

    Use only chrome in CI

--- testem.js
+++ testem.js
@@ -1,41 +1,12 @@
-/* jshint node:true*/
+/*jshint node:true*/
 module.exports = {
-  framework: 'qunit',
-  disable_watching: false,
-  serve_files: [
-    '../assets/lib.js',
+  "framework": "qunit",
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "Chrome"
   ],
-  src_files: [
-    'app/**/*.js',
-    'test/**/*.js',
-  ],
-  test_page: 'tests/index.html?hidepassed&coverage',
-  ignore_missing_launchers: false,
-  launch_in_ci: [
-    'Chrome Custom',
-  ],
-  launch_in_dev: [
-    'Chrome Custom',
-  ],
-  launchers: {
-    'Chrome Custom': {
-      exe: [
-        'google-chrome',
-        '~/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-      ],
-      args: [
-        '--user-data-dir=/tmp/testem.chrome.custom',
-        '--no-default-browser-check',
-        '--no-first-run',
-        '--ignore-certificate-errors',
-        '--test-type',
-        '--disable-extensions',
-        '--disable-web-security',
-        '--disable-renderer-backgrounding',
-        '--disable-background-timer-throttling',
-      ],
-      protocol: 'browser',
-    },
-  },
+  "launch_in_dev": [
+    "Chrome"
+  ]
 };
