daa334ee0ba68bb864e174af74e9594141a88233
Arjan Singh <arjan.singh@dollarshaveclub.com>
2017-02-25 21:39:14 -0800

    [fix] Acceptance Test Compatibility

--- tests/integration/components/triggers-change-test.js
+++ tests/integration/components/triggers-change-test.js
@@ -3,13 +3,13 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('triggers-change', {
-  integration: true
+  integration: true,
 });
 
-test('it should trigger DOM node change event on didInsertElement', function (assert) {
-  var done = assert.async();
+test('it should trigger DOM node change event on didInsertElement', function testTrigger(assert) {
+  const done = assert.async();
   this.set('x', 'initial');
-  this.render(hbs`{{ triggers-change changeTriggerInterval=1 }}`);
+  this.render(hbs`{{ triggers-change changeTriggerInterval=1 triggerUnderTest=true }}`);
   this.$('.triggers-change').on('change', () => this.set('x', 'updated via change event on DOM node'));
   Ember.run.later(() => {
     assert.equal(this.get('x'), 'updated via change event on DOM node');
