bc6fe1b1183dec3cfc4ff55932d88786105fadbe
Arjan Singh <arjan.singh@dollarshaveclub.com>
2017-02-25 21:39:14 -0800

    [fix] Acceptance Test Compatibility

--- addon/mixins/triggers-change.js
+++ addon/mixins/triggers-change.js
@@ -3,14 +3,15 @@ import Ember from 'ember';
 // Used to fix issues with autocomplete on various browsers.
 //
 
-
 export default Ember.Mixin.create({
 
   changeTriggerInterval: 2500,
 
-  triggerChange: Ember.on('didInsertElement', function () {
+  triggerChange: Ember.on('didInsertElement', function triggerChange() {
+    if (Ember.Test && !Ember.get(this, 'triggerUnderTest')) return;
+
     // Recursion.
-    Ember.run.later(this, function () {
+    Ember.run.later(() => {
       const $el = this.$();
       if (this.get('isDestroyed') || $el.length === 0) return;
       if ($el.trigger) $el.trigger('change');
@@ -18,4 +19,5 @@ export default Ember.Mixin.create({
     }, this.get('changeTriggerInterval'));
   }),
 
+  triggerUnderTest: false,
 });
---
+++ tests/acceptance/payment-method-test.js
@@ -0,0 +1,12 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('acceptance:uniform');
+
+test('visiting /payment-method', function(assert) {
+  visit('/payment-method');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/payment-method', 'it should not break acceptance tests');
+  });
+});
